ab353738b671d932d4d4264a48c65e4f67b828d6
Daniel Bork <daniel.bork@yahoo.com.br>
2026-06-16 10:58:04 -0300

    fix adjust files to commit in railway

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "../generated/prisma/client";
 import { Pool } from "pg";
 
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL!;
 
 const pool = new Pool({connectionString})
 const adapter = new PrismaPg(pool);
